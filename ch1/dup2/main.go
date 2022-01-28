52c30adb93d76adfeebdf34661d66136fffba6b3
Varun Gupta <varungupta3009@gmail.com>
2022-01-28 13:59:46 +0000

    Exercise 1.4

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -6,6 +6,7 @@
 
 // Dup2 prints the count and text of lines that appear more than once
 // in the input.  It reads from stdin or from a list of named files.
+// Exercise 1.4: Modified dup2 to print the names of all the files in which each duplicated line occurs.
 package main
 
 import (
@@ -15,32 +16,40 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string][]string)
 	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
 		}
+		countLines(f, counts, arg)
+		f.Close()
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for line, files := range counts {
+		if len(files) > 1 {
+			fmt.Printf("%s\n%s\n\n", line, files)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func countLines(f *os.File, counts map[string][]string, filename string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		if !contains(counts[input.Text()], filename) {
+			counts[input.Text()] = append(counts[input.Text()], filename)
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
