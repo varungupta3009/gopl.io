1473e2e2356af1c6fd8914ca3e8d089384926132
Varun Gupta <varungupta3009@gmail.com>
2022-01-28 13:36:21 +0000

    Exercise 1.3

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -4,17 +4,31 @@
 // See page 8.
 
 // Echo3 prints its command-line arguments.
+// Exercise 1.3: Modified to measure time efficiency b/w multiple algorithms.
 package main
 
 import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 //!+
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	start := time.Now()
+	result := strings.Join(os.Args[1:], " ")
+	fmt.Println(result)
+	fmt.Printf("Time taken for string.Join(): %dμs\n\n", time.Since(start).Microseconds())
+	start = time.Now()
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	result = s
+	fmt.Println(result)
+	fmt.Printf("Time taken for iterative algorithm: %dμs\n", time.Since(start).Microseconds())
 }
 
 //!-
